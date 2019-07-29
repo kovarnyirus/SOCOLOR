d3689803c341648857883c47dcef647dd2a0780e
kovarnyirus <kovarnyi@gmail.com>
2019-07-29 14:06:46 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,136 @@
+"use strict";
+
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var rename = require("gulp-rename");
+var csso = require("gulp-csso");
+var sass = require('gulp-sass');
+var browserSync = require("browser-sync").create();
+var svgSprite = require('gulp-svg-sprite');
+var svgmin = require('gulp-svgmin');
+var cheerio = require('gulp-cheerio');
+var replace = require('gulp-replace');
+
+gulp.task("clean", function () {
+    return del("build");
+});
+
+gulp.task('pug', function () {
+    return gulp.src('source/pug/pages/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('build'))
+        .on('end', browserSync.reload)
+});
+
+gulp.task('style', function () {
+    return gulp.src('source/sass/main.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(csso())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+    // .pipe(server.stream());
+});
+
+gulp.task('copyImg', function() {
+    return gulp.src("source/img/**/**")
+        .pipe(gulp.dest("./build/img/"))
+});
+
+gulp.task('copyJS', function() {
+    return gulp.src("source/js/**/**")
+        .pipe(gulp.dest("./build/js/"))
+});
+
+    //
+    // gulp.task("copy", function() {
+    //     return gulp.src([
+    //         "fonts/**/*.{woff,woff2}",
+    //         "img/**",
+    //         "js/**",
+    //         "*.html"
+    //     ], {
+    //         base: "."
+    //     })
+    //         .pipe(gulp.dest("build"))
+    // });
+
+gulp.task("server", function () {
+    browserSync.init({
+        server: "build/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
+    // browserSync.watch('build', browserSync.reload)
+});
+
+
+gulp.task('svg', () => {
+    return gulp.src('./source/img/svg/icon-*.svg')
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        .pipe(replace('&gt;', '>'))
+        .pipe(svgSprite({
+            shape: {
+                dimension: {         // Set maximum dimensions
+                    maxWidth: 500,
+                    maxHeight: 500
+                },
+                spacing: {         // Add padding
+                    padding: 0
+                }
+            },
+            mode: {
+                symbol: {
+                    sprite: "../sprite.svg",
+                    render: {
+                        scss: {
+                            dest: '../_sprite.scss',
+                            template: './source/sass/_sprite_template.scss'
+                        }
+                    }
+                }
+            }
+
+        }))
+
+        .pipe(gulp.dest('./build/img/svg/'));
+});
+
+
+gulp.task("watch", function () {
+    gulp.watch("source/**/*.{scss,sass}", gulp.series("style"));
+    gulp.watch("source/img/*.{png,jpg}", gulp.series("copyImg"));
+    gulp.watch("source/js/*.js", gulp.series("copyJS"));
+    gulp.watch("source/img/**/*.svg", gulp.series(["svg", "copyImg"]));
+    gulp.watch("source/pug/**/*.pug", gulp.series("pug"));
+});
+
+gulp.task('default', gulp.series(
+    gulp.parallel('pug', 'style'),
+    gulp.parallel('watch', 'server'),
+));
---
+++ source/js/main.js
@@ -0,0 +1,295 @@
+// jQuery(document).ready(function ($) {
+//     let brandSliderBtrPrew = '<button class="btn-prev brand-slider__btn-prev">\n' +
+//         '          <svg class="icon icon-arrow-left ">\n' +
+//         '            <use xlink:href="img/svg/sprite.svg#icon-arrow-left"></use>\n' +
+//         '          </svg>\n' +
+//         '        </button>';
+//     let brandSliderBtrnext = '<button class="btn-next brand-slider__btn-next">\n' +
+//         '          <svg class="icon icon-arrow-right ">\n' +
+//         '            <use xlink:href="img/svg/sprite.svg#icon-arrow-right"></use>\n' +
+//         '          </svg>\n' +
+//         '        </button>';
+//
+//     let advertisingSliderNext = '<button class="btn-next advertising__btn-next slick-arrow" style="">\n' +
+//         '              <svg class="icon icon-arrow-right ">\n' +
+//         '                <use xlink:href="img/svg/sprite.svg#icon-arrow-right"></use>\n' +
+//         '              </svg>\n' +
+//         '            </button>' ;
+//     let advertisingSliderPrev = '<button class="btn-prev advertising__btn-prev slick-arrow" style="">\n' +
+//         '              <svg class="icon icon-arrow-left ">\n' +
+//         '                <use xlink:href="img/svg/sprite.svg#icon-arrow-left"></use>\n' +
+//         '              </svg>\n' +
+//         '            </button>'
+//
+//     $('.brand-slider__list').slick({
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 6,
+//         slidesToScroll: 2,
+//         centerMode: true,
+//         variableWidth: true,
+//         dots: false,
+//         prevArrow: brandSliderBtrPrew,
+//         nextArrow: brandSliderBtrnext,
+//         arrows: false,
+//         responsive: [{
+//
+//             breakpoint: 990,
+//             settings: {
+//                 slidesToShow: 4,
+//                 arrows: false,
+//             }
+//
+//
+//         }]
+//     });
+//
+//     $('.sale-slider__list').slick({
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 6,
+//         centerMode: true,
+//         variableWidth: true,
+//         // prevArrow: $('.sale-slider__btn-prev'),
+//         // nextArrow: $('.sale-slider__btn-next'),
+//         responsive: [{
+//
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 4,
+//                 arrows: false
+//             }
+//
+//
+//         }]
+//     });
+//
+//     $('.advertising__slider').slick({
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 2,
+//         variableWidth: true,
+//         dots: false,
+//         prevArrow: advertisingSliderPrev,
+//         nextArrow: advertisingSliderNext,
+//         responsive: [{
+//
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 4
+//             }
+//         }]
+//     });
+//
+//     $('.catalog-nav__brand').slick({
+//         slidesToShow: 5,
+//         variableWidth: true,
+//         dots: false,
+//         arrows: false,
+//         responsive: [
+//             {
+//                 breakpoint: 99999,
+//                 settings: 'unslick'
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//         ]
+//     });
+//
+//
+//     $(".product-head__sliders").each(function () {
+//         var th = $(this);
+//
+//
+//         //  слайдер в  карточке товара
+//         th.find('.product-head__slider-big').slick({
+//             slidesToShow: 1,
+//             slidesToScroll: 1,
+//             dots: false,
+//             speed: 450,
+//             asNavFor: th.find('.product-head__slider-small'),
+//             infinite: false,
+//             loop: false,
+//             arrows: false,
+//
+//         });
+//         th.find('.product-head__slider-small').slick({
+//             slidesToShow: 6,
+//             slidesToScroll: 1,
+//             dots: false,
+//             speed: 450,
+//             infinite: false,
+//             loop: false,
+//             arrows: false,
+//             vertical: true,
+//             verticalSwiping: true,
+//             focusOnSelect: true,
+//             asNavFor: th.find('.product-head__slider-big'),
+//             responsive: [{
+//                 breakpoint: 1199,
+//                 settings: {
+//                     slidesToShow: 3,
+//                 }
+//             },
+//                 {
+//                     breakpoint: 767,
+//                     settings: {
+//                         slidesToShow: 4,
+//                         vertical: false,
+//                         verticalSwiping: false,
+//                     }
+//                 },
+//
+//             ]
+//         });
+//     });
+//
+//
+//     $('.product-head__slide--big a').magnificPopup({
+//         type: 'image',
+//         closeOnContentClick: true,
+//         mainClass: 'mfp-img-mobile',
+//         image: {
+//             verticalFit: true
+//         }
+//
+//     });
+//
+//
+//     $('.product-head__video-link').magnificPopup({
+//         disableOn: 700,
+//         type: 'iframe',
+//         mainClass: 'mfp-fade',
+//         removalDelay: 160,
+//         preloader: false,
+//         fixedContentPos: false
+//     });
+//
+//
+//     $('.tabs__caption').slick({
+//         dots: false,
+//         arrows: false,
+//         infinite: false,
+//         loop: false,
+//         speed: 450,
+//         slidesToShow: 1,
+//         variableWidth: true,
+//         rows: 0,
+//         responsive: [
+//             {
+//                 breakpoint: 99999,
+//                 settings: 'unslick'
+//             },
+//             {
+//                 breakpoint: 1024,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//         ]
+//     });
+//
+//     function tabscostume(tab) {
+//         $('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
+//             $(this)
+//                 .addClass('active').siblings().removeClass('active')
+//                 .closest('.' + tab).find('.' + tab + '__item').hide().removeClass('active')
+//                 .eq($(this).index()).fadeIn().addClass('active');
+//         });
+//     };
+//     tabscostume('tabs');
+//
+//
+//     $('.instagram-block__slider').slick({
+//         slidesToShow: 5,
+//         variableWidth: true,
+//         dots: false,
+//         arrows: false,
+//     });
+//
+//     $(".order-history__toggle").click(function () {
+//         $(this).closest(".order-history__item").toggleClass('order-history__item--open');
+//     });
+//
+//     $('.popup-with-move-anim').magnificPopup({
+//         type: 'inline',
+//
+//         fixedContentPos: true,
+//         fixedBgPos: true,
+//
+//         overflowY: 'auto',
+//
+//         closeBtnInside: true,
+//         preloader: false,
+//
+//         midClick: true,
+//         removalDelay: 300,
+//         mainClass: 'my-mfp-zoom-in'
+//     });
+//
+//
+//     $('input[type="tel"]').mask("+7(999) 999-9999", {});
+//
+//     $(".custom-scroll-js").mCustomScrollbar({
+//         autoHideScrollbar: true,
+//     });
+//
+//
+//     $(".custom-scroll-js3").mCustomScrollbar({
+//         autoHideScrollbar: true,
+//     });
+//     $(".custom-scroll-js2").mCustomScrollbar({
+//         autoHideScrollbar: true,
+//     });
+//
+//     $('[data-toggle="tooltip-catalog"]').tooltip({
+//         html: true,
+//         placement: 'top',
+//         template: '<div class="tooltip tooltip-catalog__wrapper" role="tooltip">' +
+//             '<div class="tooltip-arrow"></div>' +
+//             '<div class="tooltip-inner tooltip-catalog__wrapper-inner"></div></div>'
+//     });
+//
+//     // range в каталоге
+//     $(".js-range-slider").ionRangeSlider();
+//
+//
+//     $('.select2--js').select2({
+//         minimumResultsForSearch: -1
+//     });
+//
+//
+//     $('.brands__name-list').slick({
+//         slidesToShow: 5,
+//         variableWidth: true,
+//         dots: false,
+//         arrows: false,
+//         responsive: [
+//             {
+//                 breakpoint: 99999,
+//                 settings: 'unslick'
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//         ]
+//     });
+//
+//     jQuery('.aside-filter__toggle').on('click', function(e) {
+//         e.preventDefault();
+//         jQuery(this).closest('.aside-filter__group').toggleClass('aside-filter__group--active');
+//     });
+//
+// });
+//
+//
